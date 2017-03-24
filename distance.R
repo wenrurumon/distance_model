c089302a7c610c35710c309303f6d26321dbe018
進撃の雯雯老師 <askahu6@gmail.com>
2017-03-24 14:38:49 -0500

    Update distance.R

--- distance.R
+++ distance.R
@@ -1,1 +1,8 @@
+
+rm(list=ls())
 setwd('/home/zhu/rushdata/methylation_net')
+load('snpbycol.rda')
+
+gene.dist <- dist(t(gene.out))
+gene.hc <- hclust(gene.dist)
+save(gene.dist,gene.hc,gene.out,file='temp.rda')
