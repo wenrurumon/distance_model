5d440ef324484438da7767f29e709e02c0afabd2
進撃の雯雯老師 <askahu6@gmail.com>
2017-03-27 10:33:18 -0500

    Update hclust.R

--- hclust.R
+++ hclust.R
@@ -1,1 +1,12 @@
+rm(list=ls())
+setwd('/home/zhu/rushdata/methylation_net')
+load("temp.rda")
 
+cluster <- cutree(gene.hc,500)
+cluster <- lapply(unique(cluster),function(i){
+  names(which(cluster==i))
+ })
+
+data.cluster <- lapply(cluster,function(i){
+  gene.out[,colnames(gene.out)%in%i,drop=F]
+})
